f2ffae688a623ae96e2e081c66e3de403981c9b1
Sukanvisa Pearyoo <Sukanvisa.pearyoo@gmail.com>
2023-12-08 19:45:56 -0800

    Add Circle CI configuration

---
+++ src/main/java/edu/greenriver/sdev/ci_cd_labs/CiCdLabsApplication.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.ci_cd_labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdLabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdLabsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/greenriver/sdev/ci_cd_labs/CiCdLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.ci_cd_labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdLabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
